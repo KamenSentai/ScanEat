6146a922f95501469d091a70894373a966f7cbb7
AlainKRSS <cvt.alain@gmail.com>
2019-04-01 17:09:10 +0200

    feat(): initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ScanEat
+
+## Team
+
+| Name | Role |
+|-|-|
+| Alain Cao Van Truong | Full-stack developer |
+| Marie-Athénaïs Dussordet | Full-stack developer |
+| Arthur Girault | UX/UI design & marketing |
+| Yoel Hanoufa | UX/UI design & marketing |
+| Téo Migdal | UX/UI design & marketing |
---
+++ ScanEat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScanEat
+//
+//  Created by Alain on 01/04/2019.
+//  Copyright © 2019 iOSHetic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
