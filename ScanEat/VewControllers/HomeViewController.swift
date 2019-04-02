633af113761b2fa6fb3211bba277cf64a831fecd
AlainKRSS <cvt.alain@gmail.com>
2019-04-02 23:29:07 +0200

    feat(): add navigation controller

--- ScanEat/VewControllers/HomeViewController.swift
+++ ScanEat/VewControllers/HomeViewController.swift
@@ -28,13 +28,12 @@ extension UIColor {
 
 class HomeViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var registerButton: UIButton!
     @IBOutlet weak var conditionButton: UIButton!
     @IBOutlet weak var titleButton: UILabel!
     @IBOutlet weak var subtitleButton: UILabel!
+    @IBOutlet weak var plusView: UIView!
     @IBOutlet weak var verticalPlusView: UIView!
     @IBOutlet weak var horizontalPlusView: UIView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -46,15 +45,29 @@ class HomeViewController: UIViewController {
         loginButton.layer.shadowRadius = 10
         loginButton.layer.shadowOpacity = 0.25
         loginButton.backgroundColor = UIColor(rgb: 0xFFFFFF)
-        registerButton.layer.cornerRadius = 35
-        registerButton.layer.shadowColor = UIColor.black.cgColor
-        registerButton.layer.shadowOffset = CGSize(width: 0, height: 5)
-        registerButton.layer.shadowRadius = 10
-        registerButton.layer.shadowOpacity = 0.25
         conditionButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14.0)
+        plusView.layer.cornerRadius = 35
+        plusView.layer.shadowColor = UIColor.black.cgColor
+        plusView.layer.shadowOffset = CGSize(width: 0, height: 5)
+        plusView.layer.shadowRadius = 10
+        plusView.layer.shadowOpacity = 0.25
         titleButton.font = UIFont.boldSystemFont(ofSize: 26.0)
         subtitleButton.text = subtitleButton.text!.uppercased()
         verticalPlusView.layer.cornerRadius = 2
         horizontalPlusView.layer.cornerRadius = 2
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+    @IBAction func registerAction(_ sender: UIButton) {
+        let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle : nil)
+        let registerStoryboard = storyboard.instantiateViewController(withIdentifier: "RegisterViewController") as! RegisterViewController
+        navigationController?.pushViewController(registerStoryboard, animated: true)
     }
 }
