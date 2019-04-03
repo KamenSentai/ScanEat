3a06d44bba5c7af452f9063ae9685fe78b7774ee
AlainKRSS <cvt.alain@gmail.com>
2019-04-04 00:50:55 +0200

    style(): rename component

---
+++ ScanEat/Views/IngredientCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  IngredientCollectionViewCell.swift
+//  ScanEat
+//
+//  Created by Alain on 04/04/2019.
+//  Copyright © 2019 iOSHetic. All rights reserved.
+//
+
+import UIKit
+
+class IngredientCollectionViewCell: UICollectionViewCell {
+    
+}
