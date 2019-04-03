d04a897df5c825466b32c0f99b3787234a6a2916
araknyfe <ma.dussordet@gmail.com>
2019-04-03 20:40:27 +0200

    :sparkles: added scanner logic to the project

--- ScanEat/ViewController.swift
+++ ScanEat/ViewController.swift
@@ -1,20 +1,35 @@
 //
 //  ViewController.swift
-//  ScanEat
+//  test_scan
 //
-//  Created by Alain on 01/04/2019.
-//  Copyright © 2019 iOSHetic. All rights reserved.
+//  Created by Athénaïs Dussordet on 01/04/2019.
+//  Copyright © 2019 Athenais Dussordet. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    var scanner:Scanner?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        scanner = Scanner(withViewController: self, view: self.view, codeOutputHandler: self.handleCode(code:))
+        
+        if let codeScanner = scanner {
+            codeScanner.requestCaptureSessionStartRunning()
+        }
+        
     }
-
-
+    
+    func handleCode(code:String) {
+        print(code)
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        scanner?.scannerDelegate(output, didOutput: metadataObjects, from: connection)
+    }
 }
 
