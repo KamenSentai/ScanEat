586fd1e4c615011632053769225f912b4916bf08
AlainKRSS <cvt.alain@gmail.com>
2019-04-05 01:25:18 +0200

    feat(UI): add same header to suggestion page

--- ScanEat/VewControllers/SuggestionViewController.swift
+++ ScanEat/VewControllers/SuggestionViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import AlamofireImage
 
 class SuggestionViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Navigation
         navigationController?.isNavigationBarHidden = true
+        
+        // Model
+        let ref = Database.database().reference()
+        let user = Auth.auth().currentUser?.uid
+        
+        // UI
+        photoImageView.layer.cornerRadius = 22
+        photoImageView.layer.masksToBounds = true
+        
+        ref.child("users").child(user!).observeSingleEvent(of: .value, with: { (snapshot) in
+            let snapshotValue = snapshot.value as? NSDictionary
+            let image = snapshotValue?["image"] as? String
+
+            if image != nil && image != "" {
+                if let urlImage = URL(string: image!) {
+                    self.photoImageView.af_setImage(withURL: urlImage)
+                }
+            }
+        })
     }
     
     override func viewWillAppear(_ animated: Bool) {
