1c5a70cd1a84808b1383c25b7329c235b31fe20c
AlainKRSS <cvt.alain@gmail.com>
2019-04-04 13:24:36 +0200

    test(UI): merge scanner

--- ScanEat/VewControllers/ScannerViewController.swift
+++ ScanEat/VewControllers/ScannerViewController.swift
@@ -10,7 +10,16 @@ import UIKit
 import AVFoundation
 
 class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
     var scanner:Scanner?
+    let gradientLayer: CAGradientLayer = {
+        let layer = CAGradientLayer()
+        layer.colors = [
+            UIColor.red.cgColor,
+            UIColor.green.cgColor
+        ]
+        return layer
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +31,14 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
             codeScanner.requestCaptureSessionStartRunning()
         }
         
+        view.layer.addSublayer(gradientLayer)
+        gradientLayer.frame = view.bounds
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        gradientLayer.frame = view.bounds
     }
     
     func handleCode(code:String) {
@@ -31,5 +48,7 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         scanner?.scannerDelegate(output, didOutput: metadataObjects, from: connection)
     }
+    
+    
+    
 }
-
